a2e7522932ba60b579ca2b0c377b768e3365bbde
stymj010317-commits <stymj010317@gmail.com>
2026-02-01 02:00:01 +0900

    Add files via upload

---
+++ C언어/3장/예제 03-05.c
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>  
+
+int main(void)
+{
+    float ft = 1.2345678901234567890;  //유효숫자가 많은 값으로 초기화
+    double db = 1.2345678901234567890;
+
+    printf("float형 변수의 값 : %.20f\n", ft);  //소수점 이하 20자리까지 출력
+    printf("double형 변수의 값 : %.20lf\n", db);
+
+    return 0;
+}
