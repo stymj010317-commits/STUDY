d79c3a53aee484134564da400f863aca51533d71
stymj010317-commits <stymj010317@gmail.com>
2026-02-02 23:40:41 +0900

    Add files via upload

--- C언어/3장/예제 03-09.c
+++ C언어/3장/예제 03-09.c
@@ -5,8 +5,8 @@ int main(void)
 {
     int a;
 
-    scanf("%d", &a);
-    printf("입력된 값 : %d\n", a);
+    scanf("%d", &a);  //여기서 변수 a에 값 입력
+    printf("입력된 값 : %d\n", a);  //입력한 값 출력
 
     return 0;
 }
