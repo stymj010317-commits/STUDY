c5c08c1dc85e9b59ff6132a5c6634cc73520b8e2
stymj010317-commits <stymj010317@gmail.com>
2026-02-03 23:30:16 +0900

    Add files via upload

---
+++ C언어/3장/예제 03-11.c
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>  
+
+int main(void)
+{
+    char grade;  //학점을 입력할 변수
+    char name[20];  //이름을 입력할 변수
+
+    printf("학점 입력 : ");
+    scanf("%c", &grade);  //grade 변수에 학점 입력
+
+    printf("이름 입력 : ");
+    scanf("%s", name);  //name 변수에 이름 입력, & 필요 없음
+
+    printf("%s의 학점은 %c입니다.\n", name, grade);
+
+    return 0;
+}
